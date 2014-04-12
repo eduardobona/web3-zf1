d6ec60d14afc652c54e99e5f4b354089ae240b91
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2014-04-11 21:16:26 -0300

    alterando controllers e views

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,51 +1,68 @@
 <?php
+class IndexController extends Zend_Controller_Action {
+	
+	// model a ser utilizado por todos os actions
+	protected $model;
+	
+	// é executada em todos os actions
+	public function init() {
+		// model recebendo objeto Posts
+		$this->model = new Application_Model_Posts ();
+	}
+	
+	// criando index action http://localhost ou http://localhost/index ou http://localhost/index/index
+	public function indexAction() {
+		// recebendo posts do método get posts
+		$posts = $this->model->getPosts ();
+		
+		// criando variável de view (posts) os posts recebidos
+		$this->view->posts = $posts;
+	}
+	
+	// criando mostrar action http://localhost/index/mostrar
+	public function mostrarAction() {
+		// verificando se
+		// - foi pasasdo parâmetro id
+		// - e se o parâmetro é um numérico
+		if ($this->hasParam ( "id" ) and is_numeric ( $this->getParam ( "id" ) )) {
+			// recebendo parâmetro
+			$idPost = $this->getParam ( "id" );
+		} else {
+			// redireciopnando para página inicial
+			$this->_helper->redirector ( "index" );
+		}
+		
+		// rebendo post específico passando por parâmetro o id recebido
+		$post = $this->model->getPost ( $idPost );
+		
+		// verificando se post foi encontrado
+		if (! $post) {
+			// se post não foi encontrado, redirecionar para página inicial
+			$this->_helper->redirector ( "index" );
+		}
+		
+		// criando variável de view (post)o post recebido
+		$this->view->post = $post;
+		
+		$model_comentarios = new Application_Model_Comentarios();
+		$comentarios = $model_comentarios->getComentariosPorPost($idPost);
+		$this->view->comentarios = $comentarios;
+		
+		$form = new Application_Form_ComentarioForm();
+		$this->view->form = $form;
 
-class IndexController extends Zend_Controller_Action
-{
+	}
+	public function testeAction() {
+		
+		$dados = array(
+			"nome"=>"Eduardo Bona",
+			"comentario"=>"Meu comentário!",
+			"email"=>"eduardo.bona@cesumar.br"
+		);
+		
+		$model = new Application_Model_Comentarios();
+		$model->cadastrar($dados);
 
-    // model a ser utilizado por todos os actions
-    protected $model;
-    
-    // é executada em todos os actions
-    public function init()
-    {
-        // model recebendo objeto Posts
-        $this->model = new Application_Model_Posts;
-    }
-
-    // criando index action http://localhost ou http://localhost/index ou http://localhost/index/index
-    public function indexAction()
-    {
-        // recebendo posts do método get posts
-        $posts = $this->model->getPosts();
-        
-        // criando variável de view (posts) os posts recebidos
-        $this->view->posts = $posts;
-    }
-    
-    // criando mostrar action http://localhost/index/mostrar
-    public function mostrarAction(){
-        // verificando se
-        // - foi pasasdo parâmetro id
-        // - e se o parâmetro é um numérico
-        if($this->hasParam("id") and is_numeric($this->getParam("id"))){
-            // recebendo parâmetro
-            $idPost = $this->getParam("id");
-        }else{
-            // redireciopnando para página inicial
-            $this->_helper->redirector("index");
-        }
-        
-        // rebendo post específico passando por parâmetro o id recebido
-        $post = $this->model->getPost($idPost);
-        
-        // verificando se post foi encontrado
-        if(! $post){
-            // se post não foi encontrado, redirecionar para página inicial
-           $this->_helper->redirector("index");
-        }
-        
-         // criando variável de view (post)o post recebido
-        $this->view->post = $post;        
-    }
+		$this->_helper->viewRenderer->setNoRender ( true );
+	}
 }
